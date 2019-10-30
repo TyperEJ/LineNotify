babcb81f0792fe050b441a3953947382ae88a861
EJLin <z8892463@gmail.com>
2019-10-30 15:27:42 +0800

    fixed message

--- src/Message.php
+++ src/Message.php
@@ -10,6 +10,31 @@ class Message {
         $this->message['message'] = $text;
     }
 
+    public function setImageThumbnail($url)
+    {
+        $this->message['imageThumbnail'] = $url;
+    }
+
+    public function setImageFullsize($url)
+    {
+        $this->message['imageFullsize'] = $url;
+    }
+
+    public function setStickerPackageId($packageId)
+    {
+        $this->message['stickerPackageId'] = $packageId;
+    }
+
+    public function setStickerId($stickerId)
+    {
+        $this->message['stickerId'] = $stickerId;
+    }
+
+    public function serNotificationDisabled($boolean = false)
+    {
+        $this->message['notificationDisabled'] = $boolean;
+    }
+
     public function build()
     {
         return $this->message;
